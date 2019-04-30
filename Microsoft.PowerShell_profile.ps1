d8a39f995a7ffdf8d257d0392e98876ef0772059
Kristian Thaulow <kristian1990@gmail.com>
2019-04-30 19:48:49 +0200

    Updated Powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,28 @@
 function gs {
     git status
 }
+
+function gc($message) {
+    git commit -m $message
+}
+
+function gpush {
+    git push
+}
+
+function gpull {
+    git pull
+}
+
+function gaa {
+    git add .
+}
+
+function gac($message) {
+    git add .
+    git commit -m $message
+}
+
+
+
+
